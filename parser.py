4fc79e0072bb4cf9e3b4b6714fd0ebeb86607dd2
amr.essam95@hotmail.com <amr.essam95@hotmail.com>
2017-12-11 05:39:53 +0200

    making lines in the syntax tree diagram

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import re
+import re,sys
 from pptree import *
 import scanner as sc
 import tree as t
@@ -20,7 +20,7 @@ class Parser(object):
 		scanner = sc.Scanner()
 		self.tokens = scanner.run()
 		self.token_index = 0
-		self.out = open('parser_output3.txt','w')
+		self.out = open('parser_output.txt','w')
 		
 	def get_token(self):
 		""" This function get the in order token and proceed to the next token"""
@@ -32,7 +32,8 @@ class Parser(object):
 		return self.tokens[self.token_index]
 
 	def error(self):
-		print "\n\nerror occured !!!\n\n"
+		print "\n\nError Occured !!!\n\n"
+		sys.exit()
 
 
 	def match(self,expected_token,check_second = False):
@@ -47,7 +48,7 @@ class Parser(object):
 		temp = t.Node("root"," ","r",False,False)
 		root = self.stmt_sequence(temp)
 		# program = Node(node)
-		self.out.write("program found\n")
+		self.out.write("Program Found\n")
 		return root
 
 	def stmt_sequence(self,node):
@@ -55,7 +56,7 @@ class Parser(object):
 		while self.top_token()[0] == ';':
 			self.match(self.top_token()[0])
 			node.add_child(self.statement())
-		self.out.write("stmt-sequence found\n")
+		self.out.write("Statement_Sequence Found\n")
 		return node
 
 	def statement(self):
@@ -65,7 +66,7 @@ class Parser(object):
 		elif self.top_token()[0] == 'read': temp = self.read_stmt()
 		elif self.top_token()[0] == 'write': temp = self.write_stmt() 
 		else: self.error() 
-		self.out.write("statement found\n")
+		self.out.write("Statement Found\n")
 		return temp
 
 	def ifStmt(self):
@@ -80,7 +81,7 @@ class Parser(object):
 			self.match('else')
 			node = self.stmt_sequence(temp_node)
 		self.match('end')
-		self.out.write("if-stmt found\n")
+		self.out.write("IF_Statement Found\n")
 		if simple:
 			return temp_node
 		return node
@@ -91,30 +92,30 @@ class Parser(object):
 		node = self.stmt_sequence(temp_node)
 		self.match('until')
 		node.add_child(self.exp())
-		self.out.write("repeat_stmt found\n")
+		self.out.write("Repeat_Statement Found\n")
 		return node
 
 	def assign_stmt(self):
 		id = self.top_token()[0]
 		self.match('identifier',True)
 		self.match(':=')
-		temp = t.Node("assign",id,"r",True,False)
+		temp = t.Node("assign","(%s)" % id,"r",True,False)
 		temp.add_child(self.exp())
-		self.out.write("assign_stmt found\n")
+		self.out.write("Assignment_Statement Found\n")
 		return temp
 
 	def read_stmt(self):
 		self.match('read')
 		id = self.top_token()[0]
 		self.match('identifier',True)
-		self.out.write("read_stmt\n")
-		return t.Node("read",id,"r",True,False)
+		self.out.write("Read_Statement Found\n")
+		return t.Node("read","(%s)" % id,"r",True,False)
 
 	def write_stmt(self):
 		self.match('write')
 		temp = t.Node("write"," ","r",True,False)
 		temp.add_child(self.exp())
-		self.out.write("write_stmt found\n")
+		self.out.write("Write_Statement Found\n")
 		return temp
 
 	def exp(self):
@@ -122,11 +123,11 @@ class Parser(object):
 		simple = True
 		if self.top_token()[0] == '<' or self.top_token()[0] == '=':
 			simple = False
-			temp = t.Node("comparison",self.top_token()[0],"e",True,True)
+			temp = t.Node("op","(%s)" % self.top_token()[0],"e",True,True)
 			self.match(self.top_token()[0])
 			temp.add_child(temp_node)
 			temp.add_child(self.simple_exp())
-		self.out.write("exp found\n")
+		self.out.write("Expression Found\n")
 		if simple:
 			return temp_node
 		return temp
@@ -137,12 +138,12 @@ class Parser(object):
 		simple = True
 		while self.top_token()[0] == '+' or self.top_token()[0] == '-':
 			simple = False
-			temp = t.Node("op",self.top_token()[0],"e",True,True)
+			temp = t.Node("op","(%s)" % self.top_token()[0],"e",True,True)
 			temp.add_child(temp_node)
 			self.match(self.top_token()[0])
 			temp.add_child(self.term())
 			temp_node = temp
-		self.out.write("simple-exp found\n")
+		self.out.write("Simple_Expression Found\n")
 		if simple:
 			return temp_node
 		return temp
@@ -154,12 +155,12 @@ class Parser(object):
 		simple = True
 		while self.top_token()[0] == "*":
 			simple = False
-			temp = t.Node("op","*","e",True,True)
+			temp = t.Node("op","(*)","e",True,True)
 			temp.add_child(temp_node)
 			self.match(self.top_token()[0])
 			temp.add_child(self.factor())
 			temp_node = temp
-		self.out.write("term found\n")
+		self.out.write("Term Found\n")
 		if simple:
 			return temp_node
 		return temp
@@ -171,13 +172,13 @@ class Parser(object):
 			temp = self.exp()
 			self.match(')')
 		elif self.top_token()[1] == 'number':
-			temp = t.Node("number",self.top_token()[0],"e",True,True)
+			temp = t.Node("const","(%s)" % self.top_token()[0],"e",True,True)
 			self.match(self.top_token()[1],True)
 		elif self.top_token()[1] == 'identifier':
-			temp = t.Node("identifier",self.top_token()[0],"e",True,True)
+			temp = t.Node("id","(%s)" % self.top_token()[0],"e",True,True)
 			self.match(self.top_token()[1],True)
 		else:	self.error()
-		self.out.write("factor found\n")
+		self.out.write("Factor Found\n")
 		return temp
 
 parser = Parser()
--- tree.py
+++ tree.py
@@ -1,12 +1,9 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-old_x = 0
 
 class Tree:
-    # def __init__(self,val,val2 ,x_root ,y_root,scene,node_type):
     def __init__(self,node,x_root,y_root,scene):
-        # self.root = Node(val,val2,node_type,False,False) 
         self.root = node
         self.x_root = x_root
         self.y_root = y_root
@@ -17,7 +14,17 @@ class Tree:
         self.level = levels
         self.print_tree_hidden(self.root,self.x_root,self.y_root)
 
+    def get_previoud_child(self,node):
+        parent = node.parent
+        if parent:
+            node_index = parent.children.index(node)
+            if node_index == 0:
+                return None
+            return parent.children[node_index - 1]
+
     def print_tree_hidden_breadth(self,input_node):
+        width = 80
+        height = 50
         nodes = []
         stack = [input_node]
         y = 50
@@ -29,59 +36,47 @@ class Tree:
                 last_depth = current_node.depth
                 x = 50
             if current_node.print_node:
-                y = 50* (current_node.depth + 1)
+                y = height*2* (current_node.depth + 1)
+                if current_node.parent.x - 50 > x:
+                    x = current_node.parent.x - 50
+                current_node.x = x
+                current_node.y = y
                 if current_node.type == "e":
-                    self.scene.addEllipse(x,y,50,25,pen = QPen(),brush = QBrush())
+                    self.scene.addEllipse(x,y,width,height,pen = QPen(),brush = QBrush())
                 else:
-                    self.scene.addRect(x,y,50,25,pen = QPen(),brush = QBrush())
+                    self.scene.addRect(x,y,width,height,pen = QPen(),brush = QBrush())
+                previous_child = self.get_previoud_child(current_node)
+                if previous_child:
+                    if previous_child.type == "r" and current_node.type == "r":
+                        self.scene.addLine(previous_child.x + width,previous_child.y + height/2,x ,y+height/2,pen = QPen())
+                if current_node.connect:
+                    self.scene.addLine(current_node.parent.x+width/2,current_node.parent.y+height,current_node.x + width/2,current_node.y,pen = QPen())
+                if current_node.type == "r" and current_node.depth != 1 :
+                    if previous_child:
+                        if previous_child.type != "r":
+                            self.scene.addLine(current_node.parent.x+width/2,current_node.parent.y+height,current_node.x + width/2,current_node.y,pen = QPen())
+                    else:
+                        self.scene.addLine(current_node.parent.x+width/2,current_node.parent.y+height,current_node.x + width/2,current_node.y,pen = QPen())
                 font = QFont()
                 font.setPixelSize(10)
-                text = QGraphicsTextItem(str(current_node.val)+"\n"+str(current_node.val2) )
+                text = QGraphicsTextItem(str(current_node.val))
+                text2 = QGraphicsTextItem(str(current_node.val2))
                 text.setFont(font)
                 text.boundingRect()
-                text.setPos(x+5,y+1)
+                text.setPos(x+width/3.5,y+height/4)
+                text2.setFont(font)
+                text2.boundingRect()
+                text2.setPos(x+(width/3),y+height/4 + 15)
                 self.scene.addItem(text)
-                x = x + 70
+                self.scene.addItem(text2)
+                x = x + width + 30
             stack = stack[1:]
             nodes.append(current_node)
             for child in current_node.children:
+                child.parent = current_node
                 child.depth = current_node.depth + 1
                 stack.append(child)
 
-    def print_tree_hidden(self,input_node ,x ,y):
-        self.level -= 1
-        current_node = input_node
-        if current_node.print_node:
-            if current_node.type == "e":
-                self.scene.addEllipse(x,y,50,25,pen = QPen(),brush = QBrush())
-            else:
-                self.scene.addRect(x,y,50,25,pen = QPen(),brush = QBrush())
-            font = QFont()
-            font.setPixelSize(10)
-            text = QGraphicsTextItem(str(current_node.val)+"\n"+str(current_node.val2))
-            text.setFont(font)
-            text.boundingRect()
-            text.setPos(x+5,y+1)
-            self.scene.addItem(text)
-        total_x = len(current_node.children) * 100 * self.level
-        print "t ",total_x
-        for i,node in enumerate(current_node.children):
-            margin = (total_x - (len(current_node.children)*100))/(len(current_node.children))
-            print "m ",margin
-            if i < len(current_node.children)/2:
-                new_x = x-(total_x/2) + (i) * (margin + 100)
-                print "new_x : ",new_x
-            elif i > len(current_node.children)/2:
-                new_x = x-(total_x/2) + (i) * (margin + 100)
-                print "new_x : ",new_x
-            else:
-                new_x = x
-            new_y = y + 150
-            self.print_tree_hidden(node,new_x,new_y)
-            if node.connect:
-                self.scene.addLine(x + 25,y + 25,new_x + 25,new_y,pen = QPen())
-            self.level += 1
-            # print "%s\n\n"%current_node.val
     def get_levels(self,node):
         max = 0
         for child in node.children:
@@ -100,33 +95,9 @@ class Node:
         self.print_node =  print_node
         self.connect = connect
         self.depth = 0
+        self.parent = None
+        self.x = 0
+        self.y = 0
     def add_child(self,node):
         self.children.append(node)
 
-if __name__ == "__main__":
-    app = QApplication([])
-    scene = QGraphicsScene()
-    x = Tree(val = "if",val2 = " ",x_root = 500,y_root = 500,scene= scene,node_type="r")
-    x.root.add_child(Node("read"," ","r",True,False))
-    x.root.add_child(Node("if"," ","r",True,False))
-    # x.root.add_child(Node("else"," ","e",True))
-    # x.root.children[0].add_child(Node("exp"," ","e",True))
-    # x.root.children[0].add_child(Node("op"," ","e",True))
-    x.root.children[1].add_child(Node("op"," ","e",True,True))
-    x.root.children[1].add_child(Node("assign"," ","r",True,True))
-    x.root.children[1].children[0].add_child(Node("const"," ","e",True,True))
-    x.root.children[1].children[0].add_child(Node("id"," ","e",True,True))
-    x.root.children[1].add_child(Node("repeat"," ","r",True,False))
-    x.root.children[1].add_child(Node("write"," ","r",True,False))
-
-
-
-    # x.print_tree(6)
-    x.print_tree_hidden_breadth(x.root)
-    scene = x.scene
-
-    view = QGraphicsView(scene)
-    view.setRenderHint(QPainter.Antialiasing)
-    view.resize(1000, 600)
-    view.show()
-    app.exec_()
